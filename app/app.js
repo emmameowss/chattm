a2aab4e0136a0e67a7616ea184191c0655bc32b7
emmameowss <sam065819@outlook.com>
2026-05-28 23:01:12 +0200

    persistent userid (i think)

--- app/app.js
+++ app/app.js
@@ -1,17 +1,27 @@
+// persistent userid generation stuff
+let userId = localStorage.getItem("userId")
+if (!userId || userId == null) {
+    userId = crypto.randomUUID()
+    localStorage.setItem("userId", userId)
+}
+
 const socket = io('ws://localhost:3000')
 
 function sendMessage(e) {
     e.preventDefault()
     const input = document.querySelector('input')
-    if (input.value) {
-        socket.emit("message", input.value)
+    if (input.value) {  
+        socket.emit("message", {
+            userId,
+            text: input.value
+        })
         input.value = ""
     }
     input.focus()
 }
 
 socket.on('connect', () =>{
-    document.querySelector('p').textContent = `user id: ${socket.id.slice(0,5)}`
+    document.querySelector('p').textContent = `user id: ${userId.slice(0,5)}`
 })
 
 document.querySelector('form')
