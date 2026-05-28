8ca3bebb886f37eb2b6b0677e3e8dedabeeecc6d
emmameowss <sam065819@outlook.com>
2026-05-28 22:34:12 +0200

    init (i used the wrong account for git)

---
+++ README.md
@@ -0,0 +1,7 @@
+# chat™
+
+### an incredibly simple literally bare minimum chat app
+available at: https://chat.emmameowss.gay
+
+## Setup Instructions
+- tba: add this, i have no idea how to set it up yet im writing this before i've done anything else
---
+++ server/server.js
@@ -0,0 +1,25 @@
+
+// THIS CODE WORKS DONT TOUCH IT IF YOU DONT NEED TO PLEASE
+
+import {Server} from "socket.io"
+import {createServer} from "http"
+
+const httpServer = createServer()
+const io = new Server(httpServer, {
+    cors: {
+        origin: process.env.NODE_ENV === "production" ? false : 
+        ["http://localhost:5500"]
+    }
+})
+
+io.on('connection', socket => {
+
+    console.log(`User ${socket.id} connected successfully!`)
+
+    socket.on('message', data => {
+        console.log(data)
+        io.emit("message", `${socket.id.substring(0,5)}: ${data}`)
+    })
+})
+
+httpServer.listen(3000, () => console.log("Server listening on port 3000"))
