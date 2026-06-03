d0743bfef33ffa7732c729f494d5c030663d2a44
emmameowss <sam065819@outlook.com>
2026-06-03 21:13:06 +0200

    error fix

--- app/app.js
+++ app/app.js
@@ -12,6 +12,7 @@ let audioctx = null
 
 // colors
 function getNameColor(name) {
+    if (!name) return 'var(--muted)'
     if (name.toLowerCase() === 'emma') return 'hotpink'
     let hash = 0
     for (let i = 0; i < name.length; i++) {
