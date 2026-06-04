6bf677afa5611d9475d4d9ba489ce530261807a8
emmameowss <sam065819@outlook.com>
2026-06-04 22:00:51 +0200

    typo

--- app/app.js
+++ app/app.js
@@ -486,7 +486,7 @@ socket.on('commandError', (msg) => {
 // check owner status and mute chat status on connect
 socket.on('init', ({isOwner: owner, chatMuted: muted}) => {
     isOwner = owner
-    if (muted ** !isOwner) {
+    if (muted && !isOwner) {
         document.querySelector('#message-input').disabled = true
         document.querySelector('#message-form button[type="submit"]').disabled = true
         document.querySelector('#attach-btn').disabled = true
