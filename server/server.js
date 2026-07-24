13265172045c455c8d687e2ec892cf7d04a1c193
emmameowss <sam065819@outlook.com>
2026-07-24 17:45:51 +0200

    return to s3 (if aws actually reopens my account)

--- server/server.js
+++ server/server.js
@@ -99,8 +99,7 @@ const io = new Server(httpServer, {
   pingTimeout: 60000,
 });
 const s3 = new S3Client({
-  region: "auto",
-  endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
