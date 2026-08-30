366ff1aa6aa0e29a706c98e03a3b48ccc15b5e15
emmameowss <sam065819@outlook.com>
2026-08-30 18:51:19 +0200

    db path stuff

--- server/db.js
+++ server/db.js
@@ -2,7 +2,8 @@ import Database from "better-sqlite3";
 import { readFile } from "fs/promises";
 import { existsSync } from "fs";
 
-const db = new Database("chat.db");
+const dbPath = process.env.DB_PATH || "./chat.db"
+const db = new Database(dbPath);
 db.pragma("journal_mode = WAL");
 
 export { db };
